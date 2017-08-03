79676f50f1f9d2b217a7b9cdcaa66c3db90f487a
Dan Hoying <danhoying@gmail.com>
2017-08-03 15:29:51 -0700

    Initialize variables and sound samples

--- app/src/main/java/com/example/dan/memorygame/GameActivity.java
+++ app/src/main/java/com/example/dan/memorygame/GameActivity.java
@@ -1,13 +1,83 @@
 package com.example.dan.memorygame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // initialize sound variables
+    private SoundPool soundPool;
+    int sample1 = -1;
+    int sample2 = -1;
+    int sample3 = -1;
+    int sample4 = -1;
+
+    // for the UI
+    TextView textScore;
+    TextView textDifficulty;
+    TextView textWatchGo;
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button buttonReplay;
+
+    // Some variables for the thread
+    int difficultyLevel = 3;
+    // An array to hold the randomly generated sequence
+    int[] sequenceToCopy = new int[100];
+
+    private Handler myHandler;
+    // Is a sequence playing at the moment?
+    boolean playSequence = false;
+    // And which element of the sequence are we on?
+    int elementToPlay = 0;
+
+    // For checking player's answer
+    int playerResponses;
+    int playerScore;
+    boolean isResponding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        try {
+            // Create objects of the 2 required classes
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor;
+
+            // create our four fx in memory for use
+            descriptor = assetManager.openFd("sample1.ogg");
+            sample1 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("sample2.ogg");
+            sample2 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("sample3.ogg");
+            sample3 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("sample4.ogg");
+            sample4 = soundPool.load(descriptor, 0);
+        } catch (IOException e) {
+            // catch exceptions
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
